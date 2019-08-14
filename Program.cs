52218635c37153362a8de16bb1c402d7d2cc18e6
Tony Collins <tjc@eltham-college.org.uk>
2019-08-14 15:48:05 +0100

    Added support for ( )

--- Program.cs
+++ Program.cs
@@ -12,12 +12,14 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             //Evaluate("  1 + 2 ");
+           // Console.WriteLine(calculate("3 * (4 + (2 / 3) * 6 - 5)"));
+           Console.WriteLine(calculate(" 123 - (4 ^ (3 - 1) * 8 - 8 / (1 + 1) * (3 - 1))"));
+           
+            //Console.WriteLine(calculate("3 * (4 +       (2 / 3) * 6 - 5)"));
+            //Console.WriteLine(calculate("3 + 5 * 2"));
 
-            Console.WriteLine(calculate("3 * (4 +       (2 / 3) * 6 - 5)"));
-            Console.WriteLine(calculate("3 + 5 * 2"));
 
-
-            Console.WriteLine(calculate("5 - 3 * 8 / 8"));
+            //Console.WriteLine(calculate("5 - 3 * 8 / 8"));
             Console.ReadLine();
         }
 
@@ -104,12 +106,38 @@ namespace ConsoleApp2
             if(expression[expression.Length-1] !=')') express.Add(expression.Substring(lastIndex, expression.Length - lastIndex));
             return express;
         }
+        static int precedence(char c)
+        {
 
+            switch (c)
+            {
+
+                case '+':
+
+                case '-':
+
+                    return 1;
+
+                case '*':
+
+                case '/':
+
+                    return 2;
+
+                case '^':
+
+                    return 3;
+
+            }
+
+            return -1;
+
+        }
         public static List<string> ToPostFix(string expression)
         {
 
           
-            string precedence = "-+/*^";
+           
             List<string> postFixExpression = new List<string>();          
             Stack s = new Stack();
             expression = Regex.Replace(expression, @"\s+", "");       
@@ -123,16 +151,33 @@ namespace ConsoleApp2
                     postFixExpression.Add(ch);
                 }
 
+                if (ch == "(")
+                {
+                    s.Push(ch);
+                }
+                if (ch == ")")
+                {
+                    while (s.Count!=0 && s.Peek().ToString() != "(")
+                    {
+                        postFixExpression.Add(s.Pop().ToString());
+
+                    }
+                    s.Pop();
+                }
                 if (ch == "+" || ch == "-" || ch == "/" || ch == "*" || ch=="^")
                 {
-                    if (s.Count == 0)
+                    if (s.Count == 0 || s.Peek().ToString()=="(")
                     {
                         s.Push(ch);
                     }
                     else
                     {
-                        while(s.Count!=0 && precedence.IndexOf(Convert.ToChar(ch)) < precedence.IndexOf(Convert.ToChar(s.Peek())))
+                        while(s.Count!=0 
+                              && s.Peek().ToString()!="("
+           
+                              && precedence(Convert.ToChar(ch)) <= precedence(Convert.ToChar(s.Peek())))
                         {
+                          //  s.Pop();
                             postFixExpression.Add(s.Pop().ToString());
                         }
                         s.Push(ch);
