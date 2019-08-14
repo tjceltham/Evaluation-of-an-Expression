0adce21a05e42c5ce6b5b2e47ec5e7928ae3850c
Tony Collins <tjc@eltham-college.org.uk>
2019-08-14 14:13:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Collections;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Evaluate("  1 + 2 ");
+
+            Console.WriteLine(calculate("3 * (4 +       (2 / 3) * 6 - 5)"));
+            Console.WriteLine(calculate("3 + 5 * 2"));
+
+
+            Console.WriteLine(calculate("5 - 3 * 8 / 8"));
+            Console.ReadLine();
+        }
+
+        public static double calculate(string expression)
+        {
+            List<string> postfix = ToPostFix(expression);
+            Stack s = new Stack();
+            double a;
+            double b;
+        
+            foreach (var ch in postfix)
+            {               
+                if (isNumeric(ch))
+                {
+                    s.Push(ch);
+                }
+
+                if (ch == "+" || ch == "-" || ch == "/" || ch == "*" || ch =="^")
+                {
+                    a = Convert.ToDouble(s.Pop());
+                    b = Convert.ToDouble(s.Pop());
+                    if (ch == "-")
+                    {
+                        s.Push(b - a);
+                    }
+                    if (ch == "+")
+                    {
+                        s.Push(b + a);
+                    }
+                    if (ch == "/")
+                    {
+                        s.Push(b / a);
+                    }
+                    if (ch == "*")
+                    {
+                        s.Push(b * a);
+                    }
+                    if (ch=="^")
+                    {
+                        s.Push(Math.Pow(b,a));
+                    }
+                }
+
+
+            }
+  
+            return Convert.ToDouble(s.Pop());
+        }
+
+        public static Boolean isNumeric(string num)
+        {
+           Boolean canConvert=false;
+           decimal numC;
+           canConvert = decimal.TryParse(num, out numC);
+           return canConvert;
+        }
+            
+        public static List<string> SplitMaths(string expression)
+        {
+            List<string> express = new List<string>();
+            int lastIndex=0;
+
+            for(int x = 0; x< expression.Length; x++)
+            {
+                if((expression[x] == '-' && x!=0 && isNumeric(""+expression[x-1])) ||
+                   expression[x] == '+' || 
+                   expression[x] == '/' ||
+                   expression[x] == '*' ||
+                   expression[x]== '^' || 
+                   expression[x] == ')'
+                   )
+                {
+                    if(isNumeric(expression.Substring(lastIndex, x - lastIndex))) express.Add(expression.Substring(lastIndex, x-lastIndex));
+                    express.Add(expression.Substring(x,1));
+                    lastIndex = x + 1;
+                }
+                if( expression[x] == '(')
+                {
+                    express.Add(expression.Substring(x, 1));
+                    lastIndex = x + 1;
+                }
+
+            }
+            if(expression[expression.Length-1] !=')') express.Add(expression.Substring(lastIndex, expression.Length - lastIndex));
+            return express;
+        }
+
+        public static List<string> ToPostFix(string expression)
+        {
+
+          
+            string precedence = "-+/*^";
+            List<string> postFixExpression = new List<string>();          
+            Stack s = new Stack();
+            expression = Regex.Replace(expression, @"\s+", "");       
+            List<string> exp = SplitMaths(expression);
+
+            foreach (String ch in exp)
+            {
+
+                if(isNumeric(ch))
+                {
+                    postFixExpression.Add(ch);
+                }
+
+                if (ch == "+" || ch == "-" || ch == "/" || ch == "*" || ch=="^")
+                {
+                    if (s.Count == 0)
+                    {
+                        s.Push(ch);
+                    }
+                    else
+                    {
+                        while(s.Count!=0 && precedence.IndexOf(Convert.ToChar(ch)) < precedence.IndexOf(Convert.ToChar(s.Peek())))
+                        {
+                            postFixExpression.Add(s.Pop().ToString());
+                        }
+                        s.Push(ch);
+                    }
+                }                             
+            }
+            while (s.Count != 0)
+            {
+                postFixExpression.Add(s.Pop().ToString());
+            }
+            
+            return postFixExpression;
+        }
+    }
+}
